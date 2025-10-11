713c545da7ecb6cf69b3e7c82f96e1f9fc3f2a0e
Adrian <aneuville99@gmail.com>
2025-10-11 15:37:25 +0200

    Update Pipfile and Pipfile.lock for dependency management

--- backend/src/admin.py
+++ backend/src/admin.py
@@ -1,13 +1,26 @@
 import os
 from flask_admin import Admin
-from models import db, Cliente, Reserva, PlatoCarta
+from models import db, Cliente, Reserva, PlatoCarta, SeccionCarta
 from flask_admin.contrib.sqla import ModelView
 from sqlalchemy.orm.properties import RelationshipProperty
+from wtforms import SelectField
 
 
 class AdminView(ModelView):
     def __init__(self, model, *args, **kwargs):
         super().__init__(model, *args, **kwargs)
+
+
+class PlatoCartaAdmin(ModelView):
+    form_overrides = {
+        'seccion': SelectField
+    }
+    
+    form_args = {
+        'seccion': {
+            'choices': [(choice.name, choice.value) for choice in SeccionCarta]
+        }
+    }
 
 
 def setup_admin(app):
@@ -17,4 +30,4 @@ def setup_admin(app):
 
     admin.add_view(AdminView(Cliente, db.session))
     admin.add_view(AdminView(Reserva, db.session))
-    admin.add_view(AdminView(PlatoCarta, db.session))
+    admin.add_view(PlatoCartaAdmin(PlatoCarta, db.session))
